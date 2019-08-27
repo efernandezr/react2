f7f5b47cc5c56d047bd7ef6f583e8cd41ef0fd9e
efernandezr <efernandezr@gmail.com>
2019-08-26 20:56:25 -0400

    react router 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'; 
 import Main from './components/MainComponent';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
 
-
-
-
   render() {
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
